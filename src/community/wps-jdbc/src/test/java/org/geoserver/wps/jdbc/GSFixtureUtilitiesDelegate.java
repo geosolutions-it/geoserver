b135b2bb1ad9fb8d0f3853a1943e91d475f9c7a3
Andrea Aime <andrea.aime@gmail.com>
2015-09-30 16:22:28 +0200

    Adapting to 2.7.x

--- src/community/wps-jdbc/src/test/java/org/geoserver/wps/jdbc/GSFixtureUtilitiesDelegate.java
+++ src/community/wps-jdbc/src/test/java/org/geoserver/wps/jdbc/GSFixtureUtilitiesDelegate.java
@@ -12,11 +12,10 @@ import org.geotools.test.FixtureUtilities;
 import junit.framework.TestCase;
 
 /**
- * Static methods to support the implementation of tests that use fixture configuration files. See
- * {@link OnlineTestCase} and {@link OnlineTestSupport} for details. This slightly differ from
- * org.geotools.test.FixtureUtilities as it points to a different directory. This utilities delegate
- * most of its method call to FixtureUtilities except where directory location is concerned. Note:
- * Static method cannot be overridden hence this implementation.
+ * Static methods to support the implementation of tests that use fixture configuration files. See {@link OnlineTestCase} and
+ * {@link OnlineTestSupport} for details. This slightly differ from org.geotools.test.FixtureUtilities as it points to a different directory. This
+ * utilities delegate most of its method call to FixtureUtilities except where directory location is concerned. Note: Static method cannot be
+ * overridden hence this implementation.
  * 
  * @author Victor Tey, CSIRO Earth Science and Resource Engineering
  */
@@ -30,41 +29,32 @@ public class GSFixtureUtilitiesDelegate {
     }
 
     /**
-     * Return the directory containing GeoServer test fixture configuration files. This is
-     * ".geoserver" in the user home directory.
+     * Return the directory containing GeoServer test fixture configuration files. This is ".geoserver" in the user home directory.
      */
     public static File getFixtureDirectory() {
         return new File(System.getProperty("user.home") + File.separator + ".geoserver");
     }
 
     /**
-     * Return the file that should contain the fixture configuration properties. It is not
-     * guaranteed to exist.
+     * Return the file that should contain the fixture configuration properties. It is not guaranteed to exist.
      * 
      * <p>
      * 
-     * Dots "." in the fixture id represent a subdirectory path under the GeoTools configuration
-     * file directory. For example, an id <code>a.b.foo</code> would be resolved to
-     * <code>.geotools/a/b/foo.properties<code>.
+     * Dots "." in the fixture id represent a subdirectory path under the GeoTools configuration file directory. For example, an id
+     * <code>a.b.foo</code> would be resolved to <code>.geotools/a/b/foo.properties<code>.
      * 
-     * @param fixtureDirectory
-     *            the base fixture configuration file directory, typically ".geotools" in the user
-     *            home directory.
-     * @param fixtureId
-     *            the fixture id
+     * @param fixtureDirectory the base fixture configuration file directory, typically ".geotools" in the user home directory.
+     * @param fixtureId the fixture id
      */
     public static File getFixtureFile(File fixtureDirectory, String fixtureId) {
         return FixtureUtilities.getFixtureFile(fixtureDirectory, fixtureId);
     }
 
     /**
-     * Print a notice that tests are being skipped, identifying the property file whose absence is
-     * responsible.
+     * Print a notice that tests are being skipped, identifying the property file whose absence is responsible.
      * 
-     * @param fixtureId
-     *            the fixture id
-     * @param fixtureFile
-     *            the missing fixture configuration file
+     * @param fixtureId the fixture id
+     * @param fixtureFile the missing fixture configuration file
      */
     public static void printSkipNotice(String fixtureId, File fixtureFile) {
         FixtureUtilities.printSkipNotice(fixtureId, fixtureFile);
@@ -75,28 +65,22 @@ public class GSFixtureUtilitiesDelegate {
      * 
      * <p>
      * 
-     * If a fixture configuration file is not found, a notice is printed to standard output stating
-     * that tests for this fixture id are skipped.
+     * If a fixture configuration file is not found, a notice is printed to standard output stating that tests for this fixture id are skipped.
      * 
      * <p>
      * 
-     * This method allows tests that cannot extend {@link OnlineTestCase} or
-     * {@link OnlineTestSupport} because they already extend another class (for example, a
-     * non-online test framework) to access fixture configuration files in the same way that those
-     * classes do. Only basic fixture configuration loading is supported. This method does not
-     * support the extra services such as fixture caching and connection testing provided by
+     * This method allows tests that cannot extend {@link OnlineTestCase} or {@link OnlineTestSupport} because they already extend another class (for
+     * example, a non-online test framework) to access fixture configuration files in the same way that those classes do. Only basic fixture
+     * configuration loading is supported. This method does not support the extra services such as fixture caching and connection testing provided by
      * {@link OnlineTestCase} and {@link OnlineTestSupport}.
      * 
      * <p>
      * 
-     * A JUnit 4 test fixture can readily be disabled in the absence of a fixture configuration file
-     * by placing <code>Assume.assumeNotNull(FixtureUtilities.loadFixture(fixtureId))</code> or
-     * similar in its <code>@BeforeClass</code> method. JUnit 3 tests must provide their own logic,
-     * typically overriding {@link TestCase#run()} or {@link TestCase#runTest()}, or providing a
-     * suite.
+     * A JUnit 4 test fixture can readily be disabled in the absence of a fixture configuration file by placing
+     * <code>Assume.assumeNotNull(FixtureUtilities.loadFixture(fixtureId))</code> or similar in its <code>@BeforeClass</code> method. JUnit 3 tests
+     * must provide their own logic, typically overriding {@link TestCase#run()} or {@link TestCase#runTest()}, or providing a suite.
      * 
-     * @param fixtureId
-     *            the fixture id, where dots "." are converted to subdirectories.
+     * @param fixtureId the fixture id, where dots "." are converted to subdirectories.
      * @return the fixture Properties or null
      * @see OnlineTestCase
      * @see OnlineTestSupport
