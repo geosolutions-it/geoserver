0fb8322e4b4d08e55369c9ca80800e060139c55f
afabiani <alessio.fabiani@geo-solutions.it>
2016-05-13 14:30:57 +0200

    - adding more test cases

--- src/community/backup-restore/core/src/test/java/org/geoserver/backuprestore/BackupTest.java
+++ src/community/backup-restore/core/src/test/java/org/geoserver/backuprestore/BackupTest.java
@@ -16,12 +16,22 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.logging.Level;
 
+import org.geoserver.backuprestore.tasklet.CatalogBackupTasklet;
 import org.geoserver.backuprestore.utils.BackupUtils;
 import org.geoserver.backuprestore.writer.ResourceInfoAdditionalResourceWriter;
+import org.geoserver.backuprestore.writer.StyleInfoAdditionalResourceWriter;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.CoverageInfo;
 import org.geoserver.catalog.FeatureTypeInfo;
+import org.geoserver.catalog.StyleInfo;
+import org.geoserver.catalog.WorkspaceInfo;
+import org.geoserver.config.GeoServer;
 import org.geoserver.config.GeoServerDataDirectory;
+import org.geoserver.config.GeoServerInfo;
+import org.geoserver.config.LoggingInfo;
+import org.geoserver.config.SettingsInfo;
+import org.geoserver.config.util.XStreamPersister;
+import org.geoserver.config.util.XStreamPersisterFactory;
 import org.geoserver.platform.resource.Files;
 import org.geoserver.platform.resource.Paths;
 import org.geoserver.platform.resource.Resource;
@@ -29,6 +39,8 @@ import org.geoserver.platform.resource.Resources;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.batch.core.BatchStatus;
+
+import com.thoughtworks.xstream.XStream;
 
 /**
  * 
@@ -213,5 +225,87 @@ public class BackupTest extends BackupRestoreTestSupport {
         assertTrue(Resources.exists(trgTitleFtl));
         assertTrue(Resources.exists(trgHeaderFtl));
         assertTrue(!Resources.exists(trgFakeFtl));
+    }
+
+    @Test
+    public void testStyleInfoAdditionalResourceWriter() throws IOException {
+        Catalog cat = getCatalog();
+
+        GeoServerDataDirectory dd = backupFacade.getGeoServerDataDirectory();
+        GeoServerDataDirectory td = new GeoServerDataDirectory(root);
+
+        StyleInfo style = cat.getStyleByName(StyleInfo.DEFAULT_POINT);
+
+        StyleInfoAdditionalResourceWriter siarw = new StyleInfoAdditionalResourceWriter();
+        siarw.writeAdditionalResources(backupFacade, td.get(Paths.BASE), style);
+
+        Resource srcStylesDir = BackupUtils.dir(dd.get(Paths.BASE), "styles");
+        Resource trgStylesDir = BackupUtils.dir(td.get(Paths.BASE), "styles");
+
+        assertTrue(Resources.exists(srcStylesDir));
+        assertTrue(Resources.exists(trgStylesDir));
+
+        assertTrue(Resources
+                .exists(Files.asResource(new File(trgStylesDir.dir(), style.getFilename()))));
+    }
+
+    @Test
+    public void testGeoServerGlobalSettingsStorage() throws Exception {
+        Catalog cat = getCatalog();
+        GeoServer geoserver = getGeoServer();
+
+        final XStreamPersisterFactory xStreamPersisterFactory = new XStreamPersisterFactory();
+
+        CatalogBackupTasklet catalogTsklet = new CatalogBackupTasklet(backupFacade,
+                xStreamPersisterFactory);
+
+        GeoServerDataDirectory td = new GeoServerDataDirectory(root);
+
+        catalogTsklet.doWrite(geoserver.getGlobal(), td.get(Paths.BASE), "global.xml");
+        catalogTsklet.doWrite(geoserver.getSettings(), td.get(Paths.BASE), "settings.xml");
+        catalogTsklet.doWrite(geoserver.getLogging(), td.get(Paths.BASE), "logging.xml");
+
+        assertTrue(Resources
+                .exists(Files.asResource(new File(td.get(Paths.BASE).dir(), "global.xml"))));
+        assertTrue(Resources
+                .exists(Files.asResource(new File(td.get(Paths.BASE).dir(), "settings.xml"))));
+        assertTrue(Resources
+                .exists(Files.asResource(new File(td.get(Paths.BASE).dir(), "logging.xml"))));
+
+        XStreamPersister xstream = xStreamPersisterFactory.createXMLPersister();
+        xstream.setCatalog(cat);
+        xstream.setReferenceByName(true);
+        xstream.setExcludeIds();
+        XStream xp = xstream.getXStream();
+
+        GeoServerInfo gsGlobal = (GeoServerInfo) xp
+                .fromXML(new File(td.get(Paths.BASE).dir(), "global.xml"));
+
+        assertNotNull(gsGlobal);
+
+        SettingsInfo gsSettins = (SettingsInfo) xp
+                .fromXML(new File(td.get(Paths.BASE).dir(), "settings.xml"));
+
+        assertNotNull(gsSettins);
+
+        LoggingInfo gsLogging = (LoggingInfo) xp
+                .fromXML(new File(td.get(Paths.BASE).dir(), "logging.xml"));
+
+        assertNotNull(gsLogging);
+
+        assertEquals(geoserver.getGlobal(), gsGlobal);
+        assertEquals(geoserver.getSettings(), gsSettins);
+        assertEquals(geoserver.getLogging(), gsLogging);
+
+        catalogTsklet.doWrite(cat.getDefaultWorkspace(),
+                BackupUtils.dir(td.get(Paths.BASE), "workspaces"), "default.xml");
+
+        assertTrue(Resources.exists(Files.asResource(
+                new File(BackupUtils.dir(td.get(Paths.BASE), "workspaces").dir(), "default.xml"))));
+
+        WorkspaceInfo defaultWorkspace = (WorkspaceInfo) xp.fromXML(
+                new File(BackupUtils.dir(td.get(Paths.BASE), "workspaces").dir(), "default.xml"));
+
+        assertEquals(cat.getDefaultWorkspace().getName(), defaultWorkspace.getName());
     }
 }
