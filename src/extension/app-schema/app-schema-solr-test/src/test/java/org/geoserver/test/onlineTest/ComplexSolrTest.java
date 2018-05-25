6de2dd5e91a6cebe0e71f1a967bd029084f5b437
Fernando Miño <fernandomino7@gmail.com>
2018-05-25 15:58:58 -0500

    Removes data file duplications

--- src/extension/app-schema/app-schema-solr-test/src/test/java/org/geoserver/test/onlineTest/ComplexSolrTest.java
+++ src/extension/app-schema/app-schema-solr-test/src/test/java/org/geoserver/test/onlineTest/ComplexSolrTest.java
@@ -20,27 +20,21 @@ import java.io.OutputStream;
 import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.UUID;
 import java.util.logging.Level;
 
 import javax.imageio.ImageIO;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Result;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.common.SolrInputDocument;
 import org.custommonkey.xmlunit.SimpleNamespaceContext;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.custommonkey.xmlunit.XpathEngine;
@@ -55,6 +49,10 @@ import org.geoserver.catalog.impl.WorkspaceInfoImpl;
 import org.geoserver.data.test.SystemTestData;
 import org.geoserver.test.GeoServerSystemTestSupport;
 import org.geoserver.util.IOUtils;
+import org.geotools.data.solr.SolrTypeData;
+import org.geotools.data.solr.SolrTypeData.SolrTypes;
+import org.geotools.data.solr.StationData;
+import org.geotools.data.solr.StationData.Stations;
 import org.geotools.data.solr.TestsSolrUtils;
 import org.geotools.feature.NameImpl;
 import org.geotools.image.test.ImageAssert;
@@ -212,7 +210,7 @@ public final class ComplexSolrTest extends GeoServerSystemTestSupport {
         JAXBContext typesCtx = JAXBContext.newInstance(SolrTypes.class);
         Unmarshaller typesUm = typesCtx.createUnmarshaller();
         SolrTypes types = (SolrTypes) typesUm.unmarshal(ComplexSolrTest.class
-                .getResource("/data/solr_types.xml"));
+                .getResource("/test-data/appschema/solr_types.xml"));
         for (SolrTypeData adata : types.getTypes()) {
             createField(adata.getName(), adata.getType(), adata.getMulti());
         }
@@ -220,7 +218,7 @@ public final class ComplexSolrTest extends GeoServerSystemTestSupport {
         JAXBContext stationsCtx = JAXBContext.newInstance(Stations.class);
         Unmarshaller stationsUm = stationsCtx.createUnmarshaller();
         Stations stations = (Stations) stationsUm.unmarshal(ComplexSolrTest.class
-                .getResource("/data/stationsData.xml"));
+                .getResource("/test-data/appschema/stationsData.xml"));
         for (StationData adata : stations.getStations()) {
             solrClient.add(adata.toSolrDoc());
         }
@@ -249,19 +247,17 @@ public final class ComplexSolrTest extends GeoServerSystemTestSupport {
                             cache.getAbsolutePath()));
         }
         // moving schemas files to the test directory
-        moveResourceToTempDir("/schemas/meteo.xsd", "meteo.xsd");
+        moveResourceToTempDir("/test-data/appschema/meteo.xsd", "meteo.xsd");
         // moving mapping xml, changing solr url
-        moveMappingToTempDir("/mappings/" + MAPPING_FILENAME, MAPPING_FILENAME);
+        moveMappingToTempDir("/test-data/appschema/" + MAPPING_FILENAME, MAPPING_FILENAME);
     }
     
     private void moveMappingToTempDir(String inputFilePath, String OutputFilename) throws Exception {
-     // Modify datasource and copy xml
-        File xmlFile =
-                URLs.urlToFile(ComplexSolrTest.class.getResource(inputFilePath));
+        // Modify datasource and copy xml
         Document doc =
                 DocumentBuilderFactory.newInstance()
                         .newDocumentBuilder()
-                        .parse(new InputSource(new FileInputStream(xmlFile)));
+                        .parse(new InputSource(ComplexSolrTest.class.getResourceAsStream(inputFilePath)));
         Node solrDs = doc.getElementsByTagName("SolrDataStore").item(0);
         NodeList dsChilds = solrDs.getChildNodes();
         for (int i = 0; i < dsChilds.getLength(); i++) {
@@ -397,214 +393,6 @@ public final class ComplexSolrTest extends GeoServerSystemTestSupport {
         } catch (Exception exception) {
             throw new RuntimeException(
                     String.format("Error reading resource '%s' content.", resourcePath), exception);
-        }
-    }
-    
-    /** Station data model class */
-    @XmlRootElement
-    public static class StationData implements Serializable {
-        private String measurementName;
-        private String measurementTime;
-        private String measurementUnit;
-        private Integer measurementValue;
-        private String stationCode;
-        private Integer stationId;
-        private String stationLocation;
-        private String stationName;
-        private List<String> stationTag = new ArrayList<>();
-        private List<String> stationComment = new ArrayList<>();
-
-        public StationData() {}
-
-        public StationData(
-                String measurementName,
-                String measurementTime,
-                String measurementUnit,
-                Integer measurementValue,
-                String stationCode,
-                Integer stationId,
-                String stationLocation,
-                String stationName,
-                List<String> stationTag,
-                List<String> station_comment) {
-            super();
-            this.measurementName = measurementName;
-            this.measurementTime = measurementTime;
-            this.measurementUnit = measurementUnit;
-            this.measurementValue = measurementValue;
-            this.stationCode = stationCode;
-            this.stationId = stationId;
-            this.stationLocation = stationLocation;
-            this.stationName = stationName;
-            this.stationTag = stationTag;
-            this.stationComment = station_comment;
-        }
-
-        public SolrInputDocument toSolrDoc() {
-            SolrInputDocument result = new SolrInputDocument();
-            result.addField("measurement_name", this.measurementName);
-            result.addField("measurement_time", this.measurementTime);
-            result.addField("measurement_unit", this.measurementUnit);
-            result.addField("measurement_value", this.measurementValue);
-            result.addField("station_code", this.stationCode);
-            result.addField("station_id", this.stationId);
-            result.addField("station_location", this.stationLocation);
-            result.addField("station_name", this.stationName);
-            if (this.stationTag != null) {
-                for (String str1 : this.stationTag) {
-                    result.addField("station_tag", str1);
-                }
-            }
-            if (this.stationComment != null) {
-                for (String str1 : this.stationComment) {
-                    result.addField("station_comment", str1);
-                }
-            }
-
-            return result;
-        }
-
-        public String getMeasurementName() {
-            return measurementName;
-        }
-
-        public void setMeasurementName(String measurementName) {
-            this.measurementName = measurementName;
-        }
-
-        public String getMeasurementTime() {
-            return measurementTime;
-        }
-
-        public void setMeasurementTime(String measurementTime) {
-            this.measurementTime = measurementTime;
-        }
-
-        public String getMeasurementUnit() {
-            return measurementUnit;
-        }
-
-        public void setMeasurementUnit(String measurementUnit) {
-            this.measurementUnit = measurementUnit;
-        }
-
-        public Integer getMeasurementValue() {
-            return measurementValue;
-        }
-
-        public void setMeasurementValue(Integer measurementValue) {
-            this.measurementValue = measurementValue;
-        }
-
-        public String getStationCode() {
-            return stationCode;
-        }
-
-        public void setStationCode(String stationCode) {
-            this.stationCode = stationCode;
-        }
-
-        public Integer getStationId() {
-            return stationId;
-        }
-
-        public void setStationId(Integer stationId) {
-            this.stationId = stationId;
-        }
-
-        public String getStationLocation() {
-            return stationLocation;
-        }
-
-        public void setStationLocation(String stationLocation) {
-            this.stationLocation = stationLocation;
-        }
-
-        public String getStationName() {
-            return stationName;
-        }
-
-        public void setStationName(String stationName) {
-            this.stationName = stationName;
-        }
-
-        public List<String> getStationTag() {
-            return stationTag;
-        }
-
-        public void setStationTag(List<String> stationTag) {
-            this.stationTag = stationTag;
-        }
-
-        public List<String> getStationComment() {
-            return stationComment;
-        }
-
-        public void setStationComment(List<String> stationComment) {
-            this.stationComment = stationComment;
-        }
-    }
-
-    @XmlRootElement
-    public static class Stations implements Serializable {
-        private List<StationData> stations = new ArrayList<>();
-
-        public Stations() {}
-
-        public List<StationData> getStations() {
-            return stations;
-        }
-
-        public void setStations(List<StationData> stations) {
-            this.stations = stations;
-        }
-    }
-
-    @XmlRootElement
-    public static class SolrTypes implements Serializable {
-        private List<SolrTypeData> types;
-
-        public SolrTypes() {}
-
-        public List<SolrTypeData> getTypes() {
-            return types;
-        }
-
-        public void setTypes(List<SolrTypeData> types) {
-            this.types = types;
-        }
-    }
-
-    @XmlRootElement
-    public static class SolrTypeData implements Serializable {
-        private String name;
-        private String type;
-        private Boolean multi;
-
-        public SolrTypeData() {}
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public Boolean getMulti() {
-            return multi;
-        }
-
-        public void setMulti(Boolean multi) {
-            this.multi = multi;
         }
     }
     
