e09d0cfe256b983ec333c2b66a527aa78b11df5e
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-09-10 15:58:31 +0200

    [WIP] - Revert wrong commits on MasterPassword changes

--- src/main/src/main/java/org/geoserver/security/password/URLMasterPasswordProvider.java
+++ src/main/src/main/java/org/geoserver/security/password/URLMasterPasswordProvider.java
@@ -20,17 +20,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
-
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.geoserver.config.util.XStreamPersister;
-import org.geoserver.platform.GeoServerExtensions;
 import org.geoserver.platform.resource.Resource;
 import org.geoserver.platform.resource.Resource.Type;
 import org.geoserver.security.GeoServerSecurityManager;
 import org.geoserver.security.GeoServerSecurityProvider;
-import org.geoserver.security.KeyStoreProvider;
-import org.geoserver.security.KeyStoreProviderImpl;
 import org.geoserver.security.MasterPasswordProvider;
 import org.geoserver.security.SecurityUtils;
 import org.geoserver.security.config.SecurityNamedServiceConfig;
@@ -47,36 +43,31 @@ import org.jasypt.encryption.pbe.StandardPBEByteEncryptor;
 public final class URLMasterPasswordProvider extends MasterPasswordProvider {
 
     /** base encryption key */
-    // static final char[] BASE = new char[]{ 'a', 'f', '8', 'd', 'f', 's', 's', 'v', 'j', 'K',
+    //    static final char[] BASE = new char[]{ 'a', 'f', '8', 'd', 'f', 's', 's', 'v', 'j', 'K',
     // 'L',
-    // '0', 'I', 'H', '(', 'a', 'd', 'f', '2', 's', '0', '0', 'd', 's', '9', 'f', '2', 'o',
+    //        '0', 'I', 'H', '(', 'a', 'd', 'f', '2', 's', '0', '0', 'd', 's', '9', 'f', '2', 'o',
     // 'f',
-    // '(', '4', ']' };
+    //        '(', '4', ']' };
 
-    static final char[] BASE = new char[] { 'U', 'n', '6', 'd', 'I', 'l', 'X', 'T', 'Q', 'c', 'L',
-            ')', '$', '#', 'q', 'J', 'U', 'l', 'X', 'Q', 'U', '!', 'n', 'n', 'p', '%', 'U', 'r',
-            '5', 'U', 'u', '3', '5', 'H', '`', 'x', 'P', 'F', 'r', 'X' };
+    static final char[] BASE =
+            new char[] {
+                'U', 'n', '6', 'd', 'I', 'l', 'X', 'T', 'Q', 'c', 'L', ')', '$', '#', 'q', 'J', 'U',
+                'l', 'X', 'Q', 'U', '!', 'n', 'n', 'p', '%', 'U', 'r', '5', 'U', 'u', '3', '5', 'H',
+                '`', 'x', 'P', 'F', 'r', 'X'
+            };
 
     /**
-     * permutation indices, this permutation has a cycle of 169 --> more than 168 iterations have no effect
+     * permutation indices, this permutation has a cycle of 169 --> more than 168 iterations have no
+     * effect
      */
-    // static final int[] PERM = new int[]{25, 10, 5, 21, 14, 27, 23, 4, 3, 31, 16, 29, 20, 11,
+    //    static final int[] PERM = new int[]{25, 10, 5, 21, 14, 27, 23, 4, 3, 31, 16, 29, 20, 11,
     // 0, 26,
-    // 24, 22, 13, 12, 1, 8, 18, 19, 7, 2, 17, 6, 9, 28, 30, 15};
-    static final int[] PERM = new int[] { 32, 19, 30, 11, 34, 26, 3, 21, 9, 37, 38, 13, 23, 2, 18,
-            4, 20, 1, 29, 17, 0, 31, 14, 36, 12, 24, 15, 35, 16, 39, 25, 5, 10, 8, 7, 6, 33, 27, 28,
-            22 };
-
-    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    public static String randomAlphaNumeric(int count) {
-        StringBuilder builder = new StringBuilder();
-        while (count-- != 0) {
-            int character = (int) (Math.random() * ALPHA_NUMERIC_STRING.length());
-            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
-        }
-        return builder.toString();
-    }
+    //        24, 22, 13, 12, 1, 8, 18, 19, 7, 2, 17, 6, 9, 28, 30, 15};
+    static final int[] PERM =
+            new int[] {
+                32, 19, 30, 11, 34, 26, 3, 21, 9, 37, 38, 13, 23, 2, 18, 4, 20, 1, 29, 17, 0, 31,
+                14, 36, 12, 24, 15, 35, 16, 39, 25, 5, 10, 8, 7, 6, 33, 27, 28, 22
+            };
 
     URLMasterPasswordProviderConfig config;
 
@@ -89,80 +80,18 @@ public final class URLMasterPasswordProvider extends MasterPasswordProvider {
     @Override
     protected char[] doGetMasterPassword() throws Exception {
         try {
-            return readInputPassword();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * @return
-     * @throws IOException
-     */
-    protected char[] readInputPassword() throws IOException {
-        InputStream in = null;
-        try {
-            if (!this.config.reset) {
+            InputStream in = input(config.getURL(), getConfigDir());
+            try {
                 // JD: for some reason the decrypted passwd comes back sometimes with null chars
                 // tacked on
                 // MCR, was a problem with toBytes and toChar in SecurityUtils
                 // return trimNullChars(toChars(decode(IOUtils.toByteArray(in))));
-                in = input(config.getURL(), getConfigDir());
                 return toChars(decode(IOUtils.toByteArray(in)));
-            } else {
-                final char[] newPasswdConfirm = randomAlphaNumeric(64).toCharArray();
-                try {
-                    in = input(config.getURL(), getConfigDir());
-                    char[] currPasswd = toChars(decode(IOUtils.toByteArray(in)));
-                    in.close();
-
-                    // KeyStoreProvider ksProvider = getSecurityManager().getKeyStoreProvider();
-                    KeyStoreProvider ksProvider = GeoServerExtensions.bean(KeyStoreProvider.class);
-                    if (ksProvider == null) {
-                        // use default key store provider
-                        ksProvider = new KeyStoreProviderImpl();
-                    }
-
-                    ksProvider.setSecurityManager(getSecurityManager());
-                    ksProvider.prepareForMasterPasswordChange(currPasswd, newPasswdConfirm);
-
-                    if (!config.isReadOnly()) {
-                        // write it back first
-                        try {
-                            doSetMasterPassword(newPasswdConfirm);
-                        } catch (Exception e) {
-                            throw new IOException(e);
-                        } finally {
-                            config.setReset(false);
-                        }
-                    }
-
-                    // save the new Master Password config
-                    getSecurityManager().saveMasterPasswordProviderConfig(config);
-
-                    // redigest
-                    GeoServerDigestPasswordEncoder pwEncoder = getSecurityManager()
-                            .loadPasswordEncoder(GeoServerDigestPasswordEncoder.class);
-                    String masterPasswdDigest = pwEncoder.encodePassword(newPasswdConfirm, null);
-                    OutputStream fout = getSecurityManager().security()
-                            .get(getSecurityManager().MASTER_PASSWD_DIGEST_FILENAME).out();
-                    try {
-                        IOUtils.write(masterPasswdDigest, fout);
-                    } finally {
-                        fout.close();
-                    }
-
-                    // commit the password change to the keystore
-                    ksProvider.commitMasterPasswordChange();
-                } catch (Exception e) {
-                    throw new IOException(e);
-                }
-                return newPasswdConfirm;
-            }
-        } finally {
-            if (in != null) {
+            } finally {
                 in.close();
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -276,8 +205,8 @@ public final class URLMasterPasswordProvider extends MasterPasswordProvider {
             if (config.isReadOnly()) {
                 // read-only, assure we can read from url
                 try {
-                    InputStream in = input(url,
-                            manager.masterPasswordProvider().get(config.getName()));
+                    InputStream in =
+                            input(url, manager.masterPasswordProvider().get(config.getName()));
                     try {
                         in.read();
                     } finally {
