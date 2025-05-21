9f0cb10f7f233e91c67149e54bb1ad815c355392
Andrea Aime <andrea.aime@gmail.com>
2025-05-21 12:59:17 +0200

    Xerces handling

--- src/main/src/main/java/org/geoserver/util/AllowListEntityResolver.java
+++ src/main/src/main/java/org/geoserver/util/AllowListEntityResolver.java
@@ -125,6 +125,14 @@ public class AllowListEntityResolver implements EntityResolver2, Serializable {
         }
 
         try {
+            if (systemId == null) {
+                if (name != null) {
+                    LOGGER.finest("resolveEntity name: " + name);
+                    return null;
+                }
+                throw new SAXException("External entity systemId not provided");
+            }
+
             String uri;
             if (URI.create(systemId).isAbsolute()) {
                 uri = systemId;
